3de2e7b22ebbc47eb59a687f41f784ad99c43f40
Daviicode <d.ydzdavid@yahoo.com>
2019-05-16 11:29:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+const pressMeButton = document.querySelector('#pressMeButton')
+// console.log(pressMeButton)
+pressMeButton.addEventListener( 'click', function () {
+
+  // fetch jason data
+  function fetchData(){
+        var data = fetch('https://randomuser.me/api/')
+          .then( res => res.json() )
+          .then( function (data) {
+            console.log(data.result)
+
+            fullname.innerText =
+            '${data.result[0]['name']['first']}'
+            '${data.result[0]['name']['last']}'
+          } )
+        }
+
+
+})
+
 });
